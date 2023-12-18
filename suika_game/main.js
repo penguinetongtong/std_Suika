d0acdd26494ec0b6aac4d11769a90bc88c563b99
Jean <139525853+penguinetongtong@users.noreply.github.com>
2023-12-18 20:09:37 +0900

    feat: 과일 충돌 삭제구현

--- suika_game/main.js
+++ suika_game/main.js
@@ -1,4 +1,4 @@
-import { Bodies, Engine, Body, Render, Runner, World } from "matter-js";
+import { Bodies, Engine, Body, Render, Runner, World, Events, Collision } from "matter-js";
 import { FRUITS } from "./fruits";
 
 //배경화면 color
@@ -43,17 +43,22 @@ Runner.run(engine);
 
 let currentBody = null;
 let currentFruit = null;
+//움직이지 못하게 함
+let disableAction = false;
 
 //과일 떨어뜨리기
 function addFruit() {
+  //5개 과일 중 랜덤 생성
   const index = Math.floor(Math.random() * 5);
   const fruit = FRUITS[index];
 
   const body = Bodies.circle(300, 50, fruit.radius, {
+    index:index,
     isSleeping: true,
     render: {
       sprite: { texture: `${fruit.label}.png` }
     }, 
+    //튀기는 정도: 0 ~ 1 
     restitution: 0.2,
   });
 
@@ -62,9 +67,14 @@ function addFruit() {
 
   World.add(world, body);
 }
-
+//키 눌러서 작동하게 만들기
 window.onkeydown = (event) => {
+  //disableAction이 되어 있다면 아래 코드를 실행하지 않음?
+  if (disableAction) {
+    return; 
+  }
   switch (event.code) {
+    //왼쪽으로 10만큼 이동
     case "KeyA":
       Body.setPosition(currentBody, {
         x: currentBody.position.x - 10,
@@ -73,19 +83,36 @@ window.onkeydown = (event) => {
       break;
 
       case "KeyD":
+        //오른쪽으로 10만큼 이동
         Body.setPosition(currentBody, {
           x: currentBody.position.x + 10,
           y: currentBody.position.y,
         });
       break;
-      
+      //과일 떨어짐
       case "KeyS":
+        //멈춘 후 생성
         currentBody.isSleeping = false;
-
-        addFruit();
+        //키 다운이 되었을 때
+        disableAction = true;
+        //과일이 바로 생성되지 않고 1초(1000) 뒤에 생성
+        setTimeout(() => {
+          addFruit();  
+          //1초 동안 과일을 왼쪽, 오른쪽 이동 안되게 함
+          disableAction = false;
+        }, 1000);
         break;
-
   }
 }
+//충돌 시 아래 코드 실행
+Events.on(engine, "collisionStart", (event) => {
+  event.pairs.forEach((collision) => {
+    if(collision.bodyA.index === collision.bodyB.index) {
+      World.remove(world, [collision.bodyA, collision.bodyB])
+    }
+  });
+  
+
+})
 
 addFruit();
