6a11fb7070e41c051dc0f26023fbd1e951f8dc1f
lee_junr <wns2610@icloud.com>
2023-12-18 17:49:53 +0900

    feat : 게임 배경 객체 생성

--- suika_game/main.js
+++ suika_game/main.js
@@ -1,24 +1,41 @@
-import { Engine, Render, Runner, World } from "matter-js";
-import {  } from "matter-js";
+import { Bodies, Engine, Render, Runner, World } from "matter-js";
 
 const engine = Engine.create();
 const render = Render.create({
   engine,
   element: document.body,
-  option: {
+  options: {
     wireframes : false,
     background: "#F7F4C8", // background color
     width: 620,
     height: 850,
   }
 });
-// const world = engine.world;
 
-// const leftWall = Bodies.rectangle(15, 395, 30, 790,{
-//   render :{fillStyle: "#E6B143"}
-// }); // x,y,width,height 순서
+const world = engine.world;
 
-// World.add(world, [leftWall]);
+/** 왼쪽 벽 */
+const leftWall = Bodies.rectangle(15, 395, 30, 790, { // matter js 는 오브젝트의 중앙을 기준으로 물체의 위치를 특정함.
+  isStatic: true,
+  render: {fillStyle: "#E6B143"} // 왼쪽벽 색상 설정
+});
+/** 오른쪽 벽 */
+const rightWall = Bodies.rectangle(605, 395, 30, 790, { // x,y,height,width
+  isStatic: true,
+  render: {fillStyle: "#E6B143"} // 오른쪽 색상 설정
+});
+/** 바닥 벽 */
+const ground = Bodies.rectangle(310, 820, 620, 60, { // x,y,height,width
+  isStatic: true,
+  render: {fillStyle: "#E6B143"} 
+});
+/** 천장 벽 */
+const topLine = Bodies.rectangle(310, 150, 620, 2, { // x,y,height,width
+  isStatic: true,
+  render: {fillStyle: "#E6B143"}
+});
+
+World.add(world, [leftWall, rightWall, ground]);
 
 Render.run(render);
 Render.run(engine);
